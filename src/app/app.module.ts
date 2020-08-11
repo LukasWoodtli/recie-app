db7a962ae5f9f861638c79c366bbf1b0bdce9f73
Lukas Woodtli <woodtli.lukas@gmail.com>
2020-08-11 13:59:54 +0200

    Add main components and work on header (menu)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipe-book/recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
+import { RecipesComponent } from './recipe-book/recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent,
+    ShoppingListComponent,
+    IngredientComponent,
+    RecipesComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
